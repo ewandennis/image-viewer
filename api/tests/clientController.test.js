a7003b147c12cb7fd67d7ee4ca385e68bd87d64a
Ewan Dennis <ewandennis@users.noreply.github.com>
2019-06-09 09:30:25 +0100

    Client controller unit tests

---
+++ api/tests/clientController.test.js
@@ -0,0 +1,117 @@
+"use strict";
+
+const ClientController = require("../clientController");
+const protocol = require("../protocol");
+
+const mockSocketIo = () => ({
+  on: jest.fn(),
+  emit: jest.fn()
+});
+
+const mockSocket = () => ({
+  on: jest.fn(),
+  emit: jest.fn()
+});
+
+const mockImage = "still-not-very-base64-encoded";
+const mockImagefilename = "rando-pic.jpg";
+const mockImageList = [{ filename: "pic.jpg", image: mockImage }, { filename: "photo.gif", image: mockImage }];
+const mockImageListModel = () => ({
+  listImages: jest.fn().mockResolvedValue(mockImageList),
+
+  addImage: jest.fn().mockResolvedValue(mockImagefilename)
+});
+
+// eslint-disable-next-line no-unused-vars
+const findEventListener = (listeners, eventName) => listeners.find(([name, _]) => name === eventName)[1];
+
+describe("Client controller", () => {
+  let mockServer;
+  let mockModel;
+  let ctrl;
+
+  beforeEach(() => {
+    mockServer = mockSocketIo();
+    mockModel = mockImageListModel();
+    ctrl = new ClientController(mockServer, mockModel);
+  });
+
+  it("listens for socket.io connections", () => {
+    expect(mockServer.on).toHaveBeenCalledWith("connection", expect.anything());
+  });
+
+  describe("with client connection", () => {
+    const filename = "splango.jpg";
+    let connectListener;
+    let socket;
+    let onClientConnect;
+
+    beforeEach(() => {
+      connectListener = jest.fn();
+      socket = mockSocket();
+      ctrl.addConnectListener(connectListener);
+      // eslint-disable-next-line prefer-destructuring
+      onClientConnect = findEventListener(mockServer.on.mock.calls, "connection");
+      onClientConnect(socket);
+    });
+
+    it("notifies listeners when a client connects", () => {
+      expect(connectListener).toHaveBeenCalledTimes(1);
+      expect(connectListener).toHaveBeenCalledWith(socket);
+    });
+
+    it("sends each connecting client an image list", () => {
+      expect(socket.emit).toHaveBeenCalledTimes(1);
+      expect(socket.emit).toHaveBeenCalledWith(protocol.IMAGE_LIST, mockImageList);
+    });
+
+    it("sends an image list on client request", () => {
+      const onListImages = findEventListener(socket.on.mock.calls, protocol.LIST_IMAGES);
+      onListImages(socket);
+      expect(socket.emit).toHaveBeenCalledTimes(1);
+      expect(socket.emit).toHaveBeenCalledWith(protocol.IMAGE_LIST, mockImageList);
+    });
+
+    it("forwards uploaded images to the model", () => {
+      const onUploadImage = findEventListener(socket.on.mock.calls, protocol.UPLOAD_IMAGE);
+      onUploadImage({ filename, image: mockImage });
+      expect(mockModel.addImage).toHaveBeenCalledTimes(1);
+      expect(mockModel.addImage).toHaveBeenCalledWith({ filename, image: mockImage });
+    });
+
+    it("broadcasts new images after storage", () => {
+      const onUploadImage = findEventListener(socket.on.mock.calls, protocol.UPLOAD_IMAGE);
+      return onUploadImage({ filename, image: mockImage }).then(() => {
+        expect(mockServer.emit).toHaveBeenCalledWith(protocol.IMAGE_ADDED, {
+          filename: mockImagefilename,
+          image: mockImage
+        });
+      });
+    });
+
+    describe("Upload errors", () => {
+      const err = new Error("E_POORLY_FRAMED_SHOT");
+      let onUploadImage;
+
+      beforeEach(() => {
+        mockModel.addImage.mockRejectedValueOnce(err);
+        onUploadImage = findEventListener(socket.on.mock.calls, protocol.UPLOAD_IMAGE);
+
+        // Wheesht on error
+        jest.spyOn(console, 'error').mockImplementation(() => {});
+      });
+
+      it("does not broadcast failed image uploads", () => {
+        return onUploadImage({ filename, image: mockImage }).then(() => {
+          expect(mockServer.emit).not.toHaveBeenCalledWith(protocol.IMAGE_ADDED, expect.anything());
+        });
+      });
+
+      it("translates exceptions writing an image into upload error messages", () => {
+        return onUploadImage({ filename, image: mockImage }).then(() => {
+          expect(socket.emit).toHaveBeenCalledWith(protocol.UPLOAD_ERROR, err.toString());
+        });
+      });
+    });
+  });
+});
