d19b93ea3d11594c74cc3d2e98e3f532f69cde31
Ewan Dennis <ewandennis@users.noreply.github.com>
2019-06-09 15:07:04 +0100

    Serve UI from API service

--- api/index.js
+++ api/index.js
@@ -2,11 +2,18 @@
 
 const express = require('express');
 const http = require('http');
+const path = require('path');
 const SocketIoServer = require('socket.io');
 const ClientController = require('./clientController');
 const imageListModel = require('./imageListModel');
 
 const app = express();
+
+app.use(express.static(path.join(__dirname, 'public')));
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
 const httpServer = http.createServer(app);
 const socketIoServer = new SocketIoServer(httpServer);
 const clientController = new ClientController(socketIoServer, imageListModel); // eslint-disable-line no-unused-vars
